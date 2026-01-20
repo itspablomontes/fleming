30bbf803f1ced561a72c5ff2bf492af6e229a347
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2026-01-20 12:54:40 -0300

    chore: setup dev enviroments

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
-import {tanstackRouter} from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import { defineConfig } from 'vite'
@@ -10,6 +10,10 @@ export default defineConfig({
     target: 'react',
     autoCodeSplitting: true,
   })],
+  server: {
+    host: true,
+    port: 8080,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
