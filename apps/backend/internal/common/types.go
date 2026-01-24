49be8096b9e50c50018db4de188d0cf5b86f4d1c
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2026-01-24 02:18:44 -0300

    feat: update backend typing

--- apps/backend/internal/common/types.go
+++ apps/backend/internal/common/types.go
@@ -4,6 +4,8 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
+
+	"github.com/itspablomontes/fleming/pkg/protocol/types"
 )
 
 type JSONMap map[string]interface{}
@@ -15,7 +17,7 @@ func (m JSONMap) Value() (driver.Value, error) {
 	return json.Marshal(m)
 }
 
-func (m *JSONMap) Scan(value interface{}) error {
+func (m *JSONMap) Scan(value any) error {
 	if value == nil {
 		*m = nil
 		return nil
@@ -27,7 +29,7 @@ func (m *JSONMap) Scan(value interface{}) error {
 	return json.Unmarshal(bytes, &m)
 }
 
-type JSONCodes []interface{}
+type JSONCodes []types.Code
 
 func (c JSONCodes) Value() (driver.Value, error) {
 	if c == nil {
@@ -36,7 +38,7 @@ func (c JSONCodes) Value() (driver.Value, error) {
 	return json.Marshal(c)
 }
 
-func (c *JSONCodes) Scan(value interface{}) error {
+func (c *JSONCodes) Scan(value any) error {
 	if value == nil {
 		*c = nil
 		return nil
@@ -46,4 +48,25 @@ func (c *JSONCodes) Scan(value interface{}) error {
 		return errors.New("type assertion to []byte failed")
 	}
 	return json.Unmarshal(bytes, &c)
+}
+
+type JSONStrings []string
+
+func (s JSONStrings) Value() (driver.Value, error) {
+	if s == nil {
+		return nil, nil
+	}
+	return json.Marshal(s)
+}
+
+func (s *JSONStrings) Scan(value any) error {
+	if value == nil {
+		*s = nil
+		return nil
+	}
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	return json.Unmarshal(bytes, &s)
 }
